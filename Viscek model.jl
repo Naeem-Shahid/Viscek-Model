f21428a68c22ebc4cd8f24f3e9e50fb1b7ec7180
Naeem-Shahid <100057270+Naeem-Shahid@users.noreply.github.com>
2022-08-06 22:43:05 -0500

    Update Viscek model.jl

--- Viscek model.jl
+++ Viscek model.jl
@@ -1,23 +1,16 @@
 function viscek(x, y, vx, vy; L = 25, η = 0.1, v0 = 0.03, r = 1, N = 300)
-    dt = 1   
-    
-    x += vx * dt
-	y += vy * dt
-	
+    	dt = 1   
+    	x += vx * dt
+    	y += vy * dt
 	x = x .% L
 	y = y .% L
-    
-    
-    avθ = θ₀
+    	avθ = θ₀
 	for i ∈ 1:N
 		nb = ((x .- x[i]) .^2 + (y .- y[i]) .^2) .<  r^2 
 		avθ[i] = atan.(sum(sin.(θ₀[nb])),  sum(cos.(θ₀[nb])))
-    end
-    
-    θ = avθ + η * rand(N) / 2
-	
+    	end
+    	θ = avθ + η * rand(N) / 2
 	vx = v0 * cos.(θ)
 	vy = v0 * sin.(θ)
-    
-    x, y, vx, vy
+    	x, y, vx, vy
 end
